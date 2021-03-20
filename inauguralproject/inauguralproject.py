84a7c001207275d8c0e90a598bc5924a1f19ba9a
Frederik Wiese <you@example.com>
2021-03-20 16:27:25 +0100

    progress

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -19,22 +19,24 @@ def u_func(p_h, m, r, pbar, taug, taup, epsilon, phi):
   epsilon : undercut factor
   phi : Good old
   """
+  u = (m-r*p_h-taug*p_h*epsilon-taup*max(p_h*epsilon-pbar,0))**(1-phi)*p_h**phi
+  return u   
 
-   u=(m-r*p_h-taug*p_h*epsilon-taup*max(p_h*epsilon-pbar,0))**(1-phi)*p_h**phi
-   return(u) 
+# scalar optimizer function
+def u_optimiser(m, r, pbar, taug, taup, epsilon, phi):
+    """ 
+    Optimizises with respect to utility
+    """
+    def objective(p_h, m, r, pbar, taug, taup, epsilon, phi):
+        return(-u_func(p_h=p_h, m=m, r=r, pbar=pbar, taug=taug, taup=taup, epsilon=epsilon, phi=phi))  
 
-   # scalar optimizer function
-   def u_optimiser(m, r, pbar, taug, taup, epsilon, phi):
-      """ Optimizises with respect to utility"""
-       def objective(p_h, m, r, pbar, taug, taup, epsilon, phi):
-           return(-u_func(p_h=p_h, m=m, r=r, pbar=pbar, taug=taug, taup=taup, epsilon=epsilon, phi=phi))
+    sol = optimize.minimize_scalar(objective,k, method='bounded',bounds=(0,m) args =(m, r, pbar, taug, taup, epsilon, phi))
     
-    sol = optimize.minimize_scalar(objective, method="bounded", bounds=(0,m), args =(m, r, pbar, taug, taup, epsilon, phi))
-   """
-   h_star = optimal housing
-   c_star = optimal consumption
-   u_star = optimal utility
-   """ 
+    """
+    h_star = optimal housing
+    c_star = optimal consumption
+    u_star = optimal utility
+    """ 
     h_star=sol.x
     c_star=m-r*h_star-taug*h_star*epsilon-taup*max(h_star*epsilon-pbar,0)
     u_star=u_func(p_h=h_star, m=m, r=r, pbar=pbar, taug=taug, taup=taup, epsilon=epsilon, phi=phi)

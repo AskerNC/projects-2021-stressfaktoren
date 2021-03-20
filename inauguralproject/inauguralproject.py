f711b32e7de427a19b2b81ffa6c798d82006f40f
Frederik Wiese <you@example.com>
2021-03-20 18:44:54 +0100

    Q2 done

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -42,5 +42,34 @@ def u_optimiser(m, r, pbar, taug, taup, epsilon, phi):
     u_star=u_func(p_h=h_star, m=m, r=r, pbar=pbar, taug=taug, taup=taup, epsilon=epsilon, phi=phi)
     return h_star, c_star, u_star
 
-#Plot
- 
+#Function for plotting two figures
+def two_figures(x_left, y_left, title_left, xlabel_left, ylabel_left, x_right, y_right, title_right, xlabel_right, ylabel_right):
+    """
+    plots two figures side by side.
+
+    Inputs: 
+    Divided between left and right inputs for the respective plots.
+    x_left and y_left are the data which makes up the left plot and x_right and y_right does the same for the right side plot.
+    The rest of the inputs are self explanetory desripters.
+
+    Output: 
+    Two plots side by side.
+    """
+    # initialise figure
+    fig = plt.figure(figsize=(10,4))
+    # b. left plot
+    ax_left = fig.add_subplot(1,2,1)
+    ax_left.plot(x_left,y_left)
+
+    ax_left.set_title(title_left)
+    ax_left.set_xlabel(xlabel_left)
+    ax_left.set_ylabel(ylabel_left)
+
+    # c. right plot
+    ax_right = fig.add_subplot(1,2,2)
+
+    ax_right.plot(x_right, y_right)
+
+    ax_right.set_title(title_right)
+    ax_right.set_xlabel(xlabel_right)
+    ax_right.set_ylabel(ylabel_right)
